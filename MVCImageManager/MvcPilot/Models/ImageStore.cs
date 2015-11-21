132d5fa6b2adfd559971e4c80edbd451bb4de476
马鑫 <anyangmaxin@163.com>
2015-11-21 17:18:09 +0800

    根据数据库表创建model

---
+++ MVCImageManager/MvcPilot/Models/ImageStore.cs
@@ -0,0 +1,33 @@
+﻿namespace MvcPilot.Models
+{
+    /// <summary>
+    /// 图片存储实体类
+    /// </summary>
+    public class ImageStore
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public  int ImageStore_nbr { get; set; }
+
+        /// <summary>
+        /// 图片名称
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 图片类型
+        /// </summary>
+        public string MimeType { get; set; }
+
+        /// <summary>
+        /// 图片转化为二进制存入数据库
+        /// </summary>
+        public byte[] Content { get; set; }
+
+        /// <summary>
+        /// 图片所在路径
+        /// </summary>
+        public string Src { get; set; }
+    }
+}
