9fc25773e029254cadcfc7c3580e8a48c237847f
马鑫 <anyangmaxin@163.com>
2015-11-21 17:46:02 +0800

    由于我们还要处理程序与数据库之间的交流，创建一个Entity，两个方法，一是获取所有数据，另一个是为添加数据所准备:安装entityframework,设置数据上下文

---
+++ MVCImageManager/MvcPilot/DbContext/ImageStoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using MvcPilot.Models;
+
+namespace MvcPilot.DbContext
+{
+    /// <summary>
+    /// 数据库链接
+    /// </summary>
+    public class ImageStoreDbContext : System.Data.Entity.DbContext
+    {
+        public ImageStoreDbContext() : base("ImageStore")
+        {
+        }
+
+        public DbSet<ImageStore> ImageStores { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ MVCImageManager/MvcPilot/Entities/ImageStoreEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MvcPilot.DbContext;
+using MvcPilot.Models;
+
+namespace MvcPilot.Entities
+{
+    public class ImageStoreEntity
+    {
+        ImageStoreDbContext imageStoreDbContext = new ImageStoreDbContext();
+        public IEnumerable<ImageStore> GetAll()
+        {
+           
+            return imageStoreDbContext.ImageStores.ToList();
+        }
+
+        public void Inset(ImageStore imageStore)
+        {
+            imageStoreDbContext.ImageStores.Add(imageStore);
+            imageStoreDbContext.SaveChanges();
+        }
+    }
+
+
+
+}
